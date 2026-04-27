7a8e3cd72dd0b29fa65934be809c71506b87251c
OneCode182 <onecode0182@gmail.com>
2026-04-27 14:37:30 -0500

    feat(monolith): add authorName field to CreatePostRequest DTO

--- monolith/src/main/java/edu/eci/arep/twitter/dto/CreatePostRequest.java
+++ monolith/src/main/java/edu/eci/arep/twitter/dto/CreatePostRequest.java
@@ -6,5 +6,6 @@ import jakarta.validation.constraints.Size;
 public record CreatePostRequest(
     @NotBlank(message = "Content must not be blank")
     @Size(max = 140, message = "Post content must not exceed 140 characters")
-    String content
+    String content,
+    String authorName
 ) {}
