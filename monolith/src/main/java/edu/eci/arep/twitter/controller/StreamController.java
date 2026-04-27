953b2ea798eb96c364e6b39961b6fd83bb83eed0
OneCode182 <onecode0182@gmail.com>
2026-04-27 14:37:30 -0500

    chore(monolith): refactor stream controller endpoint

--- monolith/src/main/java/edu/eci/arep/twitter/controller/StreamController.java
+++ monolith/src/main/java/edu/eci/arep/twitter/controller/StreamController.java
@@ -2,6 +2,9 @@ package edu.eci.arep.twitter.controller;
 
 import edu.eci.arep.twitter.model.Post;
 import edu.eci.arep.twitter.service.PostService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
+@Tag(name = "Stream", description = "Endpoints for the public live feed")
 public class StreamController {
 
     private final PostService postService;
@@ -18,6 +22,8 @@ public class StreamController {
     }
 
     @GetMapping("/api/stream")
+    @Operation(summary = "Get the public stream", description = "Retrieves the main feed of all posts. Currently identical to /api/posts.")
+    @ApiResponse(responseCode = "200", description = "Stream retrieved successfully")
     public ResponseEntity<List<Post>> getStream() {
         return ResponseEntity.ok(postService.getAllPosts());
     }
